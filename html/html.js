f31c9ac17d93114177b0ea08ae4006bec4186ae7
phat.phamqt@gmail.com <phat.phamqt@gmail.com>
2020-09-07 16:11:39 +0700

    html

--- html/html.js
+++ html/html.js
@@ -1,5 +1,37 @@
-let l1=document.getElementById("link")
+
+let heading1=document.getElementById('h1')
+
+heading1.href="http://facebook.com"
+
+let ul=document.getElementById("li")
+
+let li="<li>Ronaldo</li><li>Messi</li><li id='nm'>Neymar</li>"
+
+ul.innerHTML +=li
+
+nm=document.getElementById("nm")
+
+nm.remove()
 
 
-let h1=document.getElementById('heading-1')
-console.log(h1.innerHTML)
+ul.addEventListener('click',function(){
+    console.log("clicked")
+})
+
+let bt=document.getElementById("button")
+
+bt.addEventListener('click', function(){
+    let add=prompt("enter player name")
+    console.log(add)
+    ul.insertAdjacentHTML('beforeend',`<li>${add}</li>`)
+})
+
+
+let ip=document.getElementById("my-input")
+ip.addEventListener('keypress', function(data){
+console.log(data.key)
+ul.insertAdjacentHTML('beforeend',`<li>${data.key}</li>`)
+})
+
+
+
